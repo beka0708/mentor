b94619973e9b178be02d6fb067e6dd333c84f7fb
beka0708 <abdulasanovbektur@gmail.com>
2023-02-20 20:52:46 +0600

    'finisheed'

--- Exercise 1/work1.py
+++ Exercise 1/work1.py
@@ -1,69 +1,42 @@
-import math
+x1 = float(input("Введите координату x1 = "))
+y1 = float(input("Введите координату y1 = "))
+x2 = float(input("Введите координату x2 = "))
+y2 = float(input("Введите координату y2 = "))
+x3 = float(input("Введите координату x3 = "))
+y3 = float(input("Введите координату y3 = "))
+print('Ваши ответы были записаны в отдельные файлы ')
 
-with open('truefalse.txt', 'w') as file:
-    file.write('')
 
+def triangle_area(*args):
+    a = ((x1 - x3) ** 2 + (y1 - y3) ** 2) ** 0.5
+    b = ((x2 - x1) ** 2 + (y2 - y1) ** 2) ** 0.5
+    c = ((x3 - x2) ** 2 + (y3 - y2) ** 2) ** 0.5
+    p = (a + b + c) / 2
+    s = (p * (p - a) * (p - b) * (p - c)) ** 0.5
+    return f'{round(s, 2)}'
+
+
+print(triangle_area(0, 0, 0, 0, 0, 0))
 with open('area.txt', 'w') as file:
-    file.write('')
+    file.write(triangle_area())
 
-try:
-    while True:
-        x1 = int(float(input('Введите координату x1: ')))
-        y1 = int(float(input('Введите координату y1: ')))
-        x2 = int(float(input('Введите координату x2: ')))
-        y2 = int(float(input('Введите координату y2: ')))
-        x3 = int(float(input('Введите координату x3: ')))
-        y3 = int(float(input('Введите координату y3: ')))
-        print('Ваши ответы были записаны в отдельные файлы ')
-
-
-        def len_sides(x1, y1, x2, y2):
-            return round(math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2), 2)
-
-
-        def len_sides2(x2, y2, x3, y3):
-            return round(math.sqrt((x3 - x2) ** 2 + (y3 - y2) ** 2), 2)
-
-
-        def len_sides3(x3, y3, x1, y1):
-            return round(math.sqrt((x1 - x3) ** 2 + (y1 - y3) ** 2), 2)
-
-
-        def triangle_area(a, b, c):
-            p = (a + b + c) / 2
-            return round(math.sqrt((p * (p - a) * (p - b) * (p - c))), 2)
-
-
-        a = len_sides(x1, y1, x2, y2)
-        b = len_sides2(x2, y2, x3, y3)
-        c = len_sides3(x3, y3, x1, y1)
-        area = triangle_area(a, b, c)
-        with open('area.txt', 'a') as file:
-            file.write(
-                f'Площадь треугольника: {area}'
-            )
-
-        if a ** 2 + b ** 2 == c ** 2:
-            with open('truefalse.txt', 'a') as file:
-                file.write(
-                    'True'
-                )
-        elif a ** 2 + c ** 2 == b ** 2:
-            with open('truefalse.txt', 'a') as file:
-                file.write(
-                    'True'
-                )
-        else:
-            if c ** 2 + b ** 2 == a ** 2:
-                with open('truefalse.txt', 'a') as file:
-                    file.write(
-                        'True'
-                    )
-            else:
-                with open('truefalse.txt', 'a') as file:
-                    file.write(
-                        'False'
-                    )
-
-except ValueError:
-    print('Вы где-то допустили ошибку ')
+a = ((x1 - x3) ** 2 + (y1 - y3) ** 2) ** 0.5
+b = ((x2 - x1) ** 2 + (y2 - y1) ** 2) ** 0.5
+c = ((x3 - x2) ** 2 + (y3 - y2) ** 2) ** 0.5
+A = a * a
+B = b * b
+C = c * c
+g = [A, B, C]
+for i in g:
+    if A == C + B:
+        with open('truefalse.txt', 'w') as file:
+            file.write(f"{True}")
+    elif B == A + C:
+        with open('truefalse.txt', 'w') as file:
+            file.write(f"{True}")
+    elif C == A + B:
+        with open('truefalse.txt', 'w') as file:
+            file.write(f"{True}")
+    else:
+        with open('truefalse.txt', 'w') as file:
+            file.write(f"{False}")
